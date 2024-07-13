580b4f17a29adeb02735187f93966b30fd0b681b
Lanacer Yasser <160516793+LanacerYasser@users.noreply.github.com>
2024-07-13 21:36:18 +0100

    Update solv.py

--- LoginChall/solv.py
+++ LoginChall/solv.py
@@ -1,1 +1,20 @@
+#you should visit the README.md file before reading this code
+
+
+ct = 'IVBYVA ERTVBA ONFR HCCRE'
+flag = ''
+key = 1
+
+for key in range(1,26):
+    for c in ct :
+        if c != ' ':
+            x = ord(c)
+            D_x = (x - key)
+            if D_x < ord('A'):
+                D_x += 26
+            flag += chr(D_x)
+        else :
+            flag += ' '
+    print(flag)
+    flag = ''
 
